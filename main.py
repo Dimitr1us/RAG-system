b66047741629f8454759e4f94b1e01748612ffca
Dimitrius <speed.Dima05@gmail.com>
2026-04-08 01:04:49 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,55 @@
+from google import genai
+import numpy as np
+import json
+
+API_KEY = "AIzaSyD7fQi3xZbIk7YujY6QsXFeuf8XTCkBiS0"
+
+client = genai.Client(api_key = API_KEY)
+
+with open("context.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+def askModel(something):
+    response = client.models.generate_content(
+        model = "gemini-3-flash-preview",
+        contents = something
+    )
+    return response.candidates[0].content.parts[0].text
+
+def getEmbedding(something):
+    context = client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=something
+    )
+    return context.embeddings[0].values
+
+def bestContext(prompt):
+    vectorPrompt = getEmbedding(prompt)
+    bestTask = data[0]["task"]
+    bestSolution = data[0]["solution"]
+    bestVector = getEmbedding(bestTask)
+    for item in data:
+        if (cosine_similarity(vectorPrompt,getEmbedding(item["task"]))>cosine_similarity(vectorPrompt,bestVector)):
+            bestTask = item["task"]
+            bestSolution = item["solution"]
+            bestVector = getEmbedding(item["task"])
+    return (bestTask,bestSolution, bestVector)
+
+task = "Напиши функцию, которая ищет максимальный"
+
+context = bestContext(task)
+
+prompt = f"""
+Реши задачу на питоне: {task}
+Для лучшего решения задания учти во внимание также данный код:
+{context[1]}
+Назад отправь только код самой задачи.
+Если решение полностью совпадает с контекстом, то всё равно отправь код назад.
+"""
+
+print(askModel(prompt))
+
+
